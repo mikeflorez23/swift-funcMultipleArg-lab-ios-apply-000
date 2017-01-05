a13498a6a1ac566540a688529273568bfd97bda5
Michael Florez <mike_florez@ymail.com>
2017-01-04 21:24:40 -0700

    done

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -7,16 +7,17 @@
  
  * [Functions](https://github.com/learn-co-curriculum/swift-functions-readme)
  * [Functions with multiple arguments](https://github.com/learn-co-curriculum/swift-funcMultipleArg-readme)
-
+ 
  */
 
 /*: question1
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
-// write your code here
+func continent(continent: String, countries: String){
+    print("\(continent) is a continent which contains \(countries) countries.")
+}
 
-
-
+continent(continent: "USA", countries: "50")
 
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
@@ -25,7 +26,7 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting(name: "Danny", "Hello")
+greeting(name: "Danny",greeting: "Hello")
 
 
 
@@ -33,7 +34,7 @@ greeting(name: "Danny", "Hello")
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
@@ -45,11 +46,11 @@ daysInMonth(month: "November", days: 30)
 /*: question4
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
-// write your code here
+func bottles(beverage: String, numBev: Int, storage: String){
+    print("\(numBev) bottles of \(beverage) on the \(storage) wall.")
+}
 
-
-
-
+bottles(beverage: "beer", numBev: 100, storage: "kitchen")
 /*:
  Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions.
  */
